5254aeab5bb58777bb91af1ee9af40af9c244de4
Akshay <bharambeakshay1997@gmail.com>
2020-12-11 01:34:30 +0530

    [Akshay] Add. Business logic with input for stock management problem

---
+++ StockAccountManagement/StockAccountManagementBusinessLogic.js
@@ -0,0 +1,83 @@
+
+const fs = require('fs');
+class Share {
+    constructor() {
+        this.content = null;
+        this.fileName = null;
+    }
+    /*
+     * @description : jsonParse
+     * @purpose:   it reads json file from given adress and convert it to script obj.
+     * @param {data}, which has data information. 
+     */
+    jsonParse(fileName) {
+        this.fileName = fileName;
+        this.content = JSON.parse(fs.readFileSync(fileName));
+
+    }
+    printReport() {
+        console.log(this.content);
+        // this.content.forEach(element => {
+        //     console.log(element);
+
+        // });
+    }
+    /*
+     * @description : value
+     * @purpose:   it calculate value of inventory
+     * @param {data}, which has data information. 
+     */
+    value() {
+        let stockSum = 0;
+        this.content.Stock.forEach(element => {
+            stockSum = stockSum + element.price * element.numberofshares
+
+        });
+        this.content.Stock.forEach(element => {
+            console.log(element.name + '  is rs  ' + element.price * element.numberofshares);
+        });
+        // console.log(' value of  ' + this.content.Stock[0].name + "  is rs " + this.content.Stock[0].price * this.content.Stock[0].numberofshares);
+        // console.log(' value of  ' + this.content.Stock[1].name + "  is rs " + this.content.Stock[1].price * this.content.Stock[1].numberofshares);
+        // console.log(' value of  ' + this.content.Stock[2].name + "  is rs " + this.content.Stock[2].price * this.content.Stock[2].numberofshares);
+        console.log('total value of stocks ' + stockSum + "rs");
+    }
+    /*
+     * @description : stockAccount
+     * @purpose:  it creat category(empty array).
+     * @param {data}, which has data information. 
+     */
+
+    stockAccount(category = null) {
+
+        this.content[category] = [];
+
+    }
+    /*
+     * @description : buyShares
+     * @purpose:   it add elements to account
+     * @param {data}, which has data information. 
+     */
+    buyShares(category, name, numberOfShares, price) {
+
+        this.content[category].push({
+            "name": name,
+            "numberOfShares": numberOfShares,
+            "price": price,
+        })
+
+    }
+
+    /*
+     * @description : saveFile
+     * @purpose:   it convert script object and save it to json file.
+     * @param {data}, which has data information. 
+     */
+    save() {
+        fs.writeFileSync(this.fileName, JSON.stringify(this.content));
+    }
+}
+// exporting class
+module.exports = Share;
+
+
+
---
+++ StockAccountManagement/StockManagementMain.js
@@ -0,0 +1,40 @@
+const readline = require('readline-sync');
+const stockAccountMan = require('./StockAccountManagementBusinessLogic');
+let stock = new stockAccountMan();
+stock.jsonParse('./StockAccount.json');
+
+//stock.value();
+// stock.printReport();
+
+let flag = true;
+
+while (flag) {
+    let choice = readline.questionInt(`enter 1. to buy shares                2. print the shares report      
+      3. calculations of stocks       4. exit`)
+    switch (choice) {
+        case 1:
+            let addCategory = readline.question("enter name of category:");
+            let company = readline.question("enter name of company: ");
+            let numberOfShares = readline.question("enter number of shares: ");
+            let price = readline.question("enter price: ");
+            stock.stockAccount(addCategory);
+            stock.buyShares(addCategory, company, numberOfShares, price);
+            stock.save();
+            break;
+
+        case 2:
+            console.log("Printing the report: ");
+            stock.printReport();
+            break;
+        case 3:
+            console.log("printing values: ")
+            stock.value();
+            break;
+
+        case 4:
+            flag = false;
+            process.exit(1)
+        default:
+            console.log('wrong choice');
+    }
+}
